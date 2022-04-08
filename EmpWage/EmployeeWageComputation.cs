96b282ab76c994db37f823673530b97e77c12a01
tomeshsahu <tomeshwarsahu1980@gmail.com>
2022-04-08 10:59:50 +0530

    [TOMESHWAR]ADD:- EMPLOYEE WAGE CALCULATION

--- EmpWage/EmployeeWageComputation.cs
+++ EmpWage/EmployeeWageComputation.cs
@@ -9,12 +9,23 @@ namespace EmpWage
     public class EmployeeWageComputation
     {
         int EmpFullTime = 1;
+        int FullTimeWorkingHrs = 8;
+        int PartTimeWorkingHrs = 4;
+        int EmpWorkingHrs = 20;
         public void Tomesh()
         {
            Random random=new Random();
             int Check = random.Next(0, 2);
             if (Check == EmpFullTime)
-                Console.WriteLine("Present");
+            {
+                int Salary = (FullTimeWorkingHrs * EmpWorkingHrs);
+                Console.WriteLine("Employee is Working Fulltime" + "Salary" +" " + Salary);
+            }
+            else if(Check == PartTimeWorkingHrs)
+            {
+                int Salary = (PartTimeWorkingHrs * EmpWorkingHrs);
+                Console.WriteLine("Employee is Working PartTime" + "Salary"+ " " + Salary);
+            }
             else
                 Console.WriteLine("Absent");
         }
